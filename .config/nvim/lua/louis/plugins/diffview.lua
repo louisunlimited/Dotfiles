5f768bd26d8dc47526fb4a98cafc82c4e9a57cfe
Louis Qian <louisqy26@gmail.com>
2026-03-18 18:54:23 -0700

    feat(nvim): add diffview for viewing diffs

---
+++ .config/nvim/lua/louis/plugins/diffview.lua
@@ -0,0 +1,19 @@
+return {
+  "sindrets/diffview.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    local diffview = require("diffview")
+
+    diffview.setup()
+
+    -- set keymaps
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>gd", "<cmd>DiffviewOpen<cr>", { desc = "Open diff view" })
+    keymap.set("n", "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", { desc = "File history (current file)" })
+    keymap.set("n", "<leader>gH", "<cmd>DiffviewFileHistory<cr>", { desc = "File history (repo)" })
+    keymap.set("n", "<leader>gc", "<cmd>DiffviewClose<cr>", { desc = "Close diff view" })
+  end,
+}
