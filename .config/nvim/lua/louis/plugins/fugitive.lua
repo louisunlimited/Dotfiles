7ddaf44d51ee122bb5965b19f4760da011f4764c
Louis Qian <louisqy26@gmail.com>
2026-03-18 19:28:17 -0700

    chore(nvim): set up local q for fugitive to close

--- .config/nvim/lua/louis/plugins/fugitive.lua
+++ .config/nvim/lua/louis/plugins/fugitive.lua
@@ -4,6 +4,13 @@ return {
     local keymap = vim.keymap
 
     keymap.set("n", "<leader>gs", "<cmd>G<cr>", { desc = "Git status" })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "fugitive",
+      callback = function(ev)
+        vim.keymap.set("n", "q", "<cmd>q<cr>", { buffer = ev.buf, desc = "Close fugitive" })
+      end,
+    })
     keymap.set("n", "<leader>gp", "<cmd>G push<cr>", { desc = "Git push" })
     keymap.set("n", "<leader>gl", "<cmd>G pull<cr>", { desc = "Git pull" })
   end,
