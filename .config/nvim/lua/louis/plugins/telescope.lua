0cf8cd94574c767fdb48385a2aa30568aa5dcc72
Louis Qian <louisqy26@gmail.com>
2026-03-18 18:28:21 -0700

    feat(nvim): allow telescope to find hidden files

--- .config/nvim/lua/louis/plugins/telescope.lua
+++ .config/nvim/lua/louis/plugins/telescope.lua
@@ -21,6 +21,11 @@ return {
           },
         },
       },
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+      },
     })
 
     telescope.load_extension("fzf")
