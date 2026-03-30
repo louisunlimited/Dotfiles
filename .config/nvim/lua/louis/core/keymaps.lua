4eb2e6712fc6107f0df3281881a4a9be17cd8c92
Louis Qian <louisqy26@gmail.com>
2026-03-30 14:11:08 -0700

    feat(nvim): add keybind for switching tabs

--- .config/nvim/lua/louis/core/keymaps.lua
+++ .config/nvim/lua/louis/core/keymaps.lua
@@ -3,3 +3,8 @@ vim.g.mapleader = " "
 local keymap = vim.keymap
 
 keymap.set("i", "jk", "<ESC>", {desc = "Exit insert mode with jk"})
+
+keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to below window" })
+keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to above window" })
+keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
